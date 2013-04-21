65607cd86a5129ca06be32abdf228ea3ff927e2b
Jonathan Frawley <jonathanfrawley@gmail.com>
2013-04-21 20:39:49 +0100

    Added logging.

--- src/spider-hard/main.lua
+++ src/spider-hard/main.lua
@@ -1,3 +1,10 @@
+function ObjectNew(o, self)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
 Sprite = {image="", 
     pos={x=0, y=0},
     rot=0,
@@ -6,10 +13,7 @@ Sprite = {image="",
     currentLeg=0}
 
 function Sprite:new(o)
-    o = o or {}
-    setmetatable(o, self)
-    self.__index = self
-    return o
+    return ObjectNew(o, self)
 end
 
 function Sprite:init()
@@ -51,7 +55,6 @@ function Target:setTarget(spritePos, relativePos)
 end
 
 Spider = Sprite:new()
-Man = Sprite:new()
 
 function Spider:init()
     Sprite.init(self)
@@ -64,7 +67,63 @@ function Spider:init()
     self.legs[5] = {x=-26, y=10}
     self.legs[6] = {x=-26, y=-9}
     self.legs[7] = {x=-13, y=-24}
+    -- TODO: Randomise this
+    self.weakestLeg = 0
+    self.legHealth = {}
+    for i=0,9 do
+        self.legHealth[i] = 1
+    end
 end
+
+Man = Sprite:new()
+
+function Man:fire(spider, leg)
+    if(spider.weakestLeg == leg) then
+        -- We have a successful hit.
+        spider.legHealth[leg] = 0
+        table.insert(events, "SPIDER_LEG_DEAD")
+    end
+    -- We need to do something interesting here like show if they are close.
+end
+
+
+--Love2D Logging
+Logger = { logs={}, 
+    maxLogs=100,
+    startPos={x=0,y=0},
+    logYDelta=14,
+    visible=true}
+
+function Logger:new(o)
+    return ObjectNew(o, self)
+end
+
+function Logger:insert(msg)
+    while table.getn(self.logs) > self.maxLogs do
+        table.remove()
+    end
+    table.insert(self.logs, msg)
+end
+
+function Logger:d(msg)
+    self:insert(os.date("%c") .. " DEBUG: " .. msg)
+end
+
+function Logger:e(msg)
+    self:insert(os.date("%c") .. " ERROR: " .. msg)
+end
+
+function Logger:draw()
+    if self.visible == true then
+        logY = self.startPos.y
+        for i = table.getn(self.logs), 1, -1 do
+            love.graphics.print(self.logs[i], 0, logY)
+            logY = logY + self.logYDelta
+        end
+    end
+end
+
+--
 
 function checkQuit()
     if love.keyboard.isDown("escape") then
@@ -89,7 +148,13 @@ function love.draw()
     man:draw()
     target:draw()
     ui()
-    love.graphics.print(string.format("Current Leg. %d ", target.currentLeg), 0, 0)
+    while table.getn(events) > 0 do
+        event = table.remove(events)
+        if event == "SPIDER_LEG_DEAD" then
+            love.graphics.print("SPIDER_LEG_DEAD", 0, 0)
+        end
+    end
+    logger:draw()
 end
 
 -- Do some loading here
@@ -107,11 +172,15 @@ function love.load()
     love.graphics.setBackgroundColor(155, 100, 100)
     -- Howto load sound
     bounce = loadSound("bounce.wav")
+    events = {}
+    logger = Logger:new()
 end
 
 function love.keypressed(key)
     leg = target.currentLeg
-    if key == "1" then
+    if key == "`" then
+        logger.visible = not logger.visible
+    elseif key == "1" then
         leg = leg + 1
         if(leg > 7) then
             leg = 0
@@ -121,6 +190,11 @@ function love.keypressed(key)
         if(leg < 0) then
             leg = 7
         end
+    elseif key == "return" then
+        man:fire(spider)
+        logger:d("Return Pressed")
+        logger:d("Blah Blah")
+        logger:d("Blah Blah")
     end
     target.currentLeg = leg
 end
