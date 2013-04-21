61c5348c43772b5b24cf3d50c205ddacc8d3a9f7
Jonathan Frawley <jonathanfrawley@gmail.com>
2013-04-21 21:59:37 +0100

    Animations for spiders for good bad and middle

--- src/spider-hard/main.lua
+++ src/spider-hard/main.lua
@@ -1,3 +1,5 @@
+require("AnAL")
+
 function ObjectNew(o, self)
     o = o or {}
     setmetatable(o, self)
@@ -10,7 +12,7 @@ Sprite = {image="",
     rot=0,
     scale={x=1, y=1},
     halfDims={w=0, h=0},
-    currentLeg=0}
+    currentLeg=1}
 
 function Sprite:new(o)
     return ObjectNew(o, self)
@@ -46,7 +48,7 @@ Target = Sprite:new()
 
 function Target:init()
     Sprite.init(self)
-    self.currentLeg = 0
+    target:setTarget(spider.centrePos, spider.legs[target.currentLeg])
 end
 
 function Target:setTarget(spritePos, relativePos)
@@ -59,18 +61,18 @@ Spider = Sprite:new()
 function Spider:init()
     Sprite.init(self)
     self.legs = {}
-    self.legs[0] = {x=15, y=-24}
-    self.legs[1] = {x=24, y=-9}
-    self.legs[2] = {x=24, y=10}
-    self.legs[3] = {x=15, y=22}
-    self.legs[4] = {x=-18, y=22}
-    self.legs[5] = {x=-26, y=10}
-    self.legs[6] = {x=-26, y=-9}
-    self.legs[7] = {x=-13, y=-24}
+    self.legs[1] = {x=15, y=-24}
+    self.legs[2] = {x=24, y=-9}
+    self.legs[3] = {x=24, y=10}
+    self.legs[4] = {x=15, y=22}
+    self.legs[5] = {x=-18, y=22}
+    self.legs[6] = {x=-26, y=10}
+    self.legs[7] = {x=-26, y=-9}
+    self.legs[8] = {x=-13, y=-24}
     -- TODO: Randomise this
-    self.weakestLeg = 0
+    self.weakestLeg = 1
     self.legHealth = {}
-    for i=0,9 do
+    for i=1,8 do
         self.legHealth[i] = 1
     end
 end
@@ -78,10 +80,17 @@ end
 Man = Sprite:new()
 
 function Man:fire(spider, leg)
+    logger:d(string.format("In man:fire leg is %d weakest is %d", leg, spider.weakestLeg))
+    local nearDelta = 1
     if(spider.weakestLeg == leg) then
         -- We have a successful hit.
         spider.legHealth[leg] = 0
         table.insert(events, "SPIDER_LEG_DEAD")
+    elseif(((spider.weakestLeg >= (leg - nearDelta)) and (spider.weakestLeg <= (leg + nearDelta)))
+        or (spider.weakestLeg==1 and leg==8) or (spider.weakestLeg==8 and leg==1)) then
+        table.insert(events, "SPIDER_LEG_ALMOST_DEAD")
+    else
+        table.insert(events, "SPIDER_LEG_MISS")
     end
     -- We need to do something interesting here like show if they are close.
 end
@@ -99,7 +108,7 @@ function Logger:new(o)
 end
 
 function Logger:insert(msg)
-    while table.getn(self.logs) > self.maxLogs do
+    while #self.logs > self.maxLogs do
         table.remove()
     end
     table.insert(self.logs, msg)
@@ -116,7 +125,7 @@ end
 function Logger:draw()
     if self.visible == true then
         logY = self.startPos.y
-        for i = table.getn(self.logs), 1, -1 do
+        for i=#self.logs, 1, -1 do
             love.graphics.print(self.logs[i], 0, logY)
             logY = logY + self.logYDelta
         end
@@ -148,11 +157,31 @@ function love.draw()
     man:draw()
     target:draw()
     ui()
-    while table.getn(events) > 0 do
+    while #events > 0 do
         event = table.remove(events)
+        local animTime = 0.5
         if event == "SPIDER_LEG_DEAD" then
             love.graphics.print("SPIDER_LEG_DEAD", 0, 0)
+            local img = love.graphics.newImage("anim_spider_good.png")
+            local anim = newAnimation(img, 64, 64, animTime, 0)
+            anim:setMode("once")
+            table.insert(anims, {pos=spider.pos, anim=anim})
+        elseif event == "SPIDER_LEG_ALMOST_DEAD" then
+            love.graphics.print("SPIDER_LEG_ALMOST_DEAD", 0, 0)
+            local img = love.graphics.newImage("anim_spider_med.png")
+            local anim = newAnimation(img, 64, 64, animTime, 0)
+            anim:setMode("once")
+            table.insert(anims, {pos=spider.pos, anim=anim})
+        elseif event == "SPIDER_LEG_MISS" then
+            love.graphics.print("SPIDER_LEG_MISS", 0, 0)
+            local img = love.graphics.newImage("anim_spider_bad.png")
+            local anim = newAnimation(img, 64, 64, animTime, 0)
+            anim:setMode("once")
+            table.insert(anims, {pos=spider.pos, anim=anim})
         end
+    end
+    for i = 1, #anims do
+        anims[i].anim:draw(anims[i].pos.x - 15, anims[i].pos.y - 15)
     end
     logger:draw()
 end
@@ -162,9 +191,9 @@ function love.load()
     -- constants
     PI = 3.14159
     -- create some sprites.
-    spider = Spider:new{pos={x=50.0,y=50.0}, image="spider.png"}
+    spider = Spider:new{pos={x=450.0,y=225.0}, image="spider.png"}
     spider:init()
-    man = Man:new{pos={x=400.0,y=50.0},image="man.png"}
+    man = Man:new{pos={x=250.0,y=200.0},image="man.png"}
     man:init()
     target = Target:new{pos={x=100,y=200},image="target.png"}
     target:init()
@@ -174,6 +203,7 @@ function love.load()
     bounce = loadSound("bounce.wav")
     events = {}
     logger = Logger:new()
+    anims = {}
 end
 
 function love.keypressed(key)
@@ -182,27 +212,28 @@ function love.keypressed(key)
         logger.visible = not logger.visible
     elseif key == "1" then
         leg = leg + 1
-        if(leg > 7) then
-            leg = 0
+        if(leg > 8) then
+            leg = 1
         end
     elseif key == "2" then
         leg = leg - 1
-        if(leg < 0) then
-            leg = 7
+        if(leg < 1) then
+            leg = 8
         end
     elseif key == "return" then
-        man:fire(spider)
-        logger:d("Return Pressed")
-        logger:d("Blah Blah")
-        logger:d("Blah Blah")
+        man:fire(spider, target.currentLeg)
     end
     target.currentLeg = leg
+    target:setTarget(spider.centrePos, spider.legs[target.currentLeg])
 end
-
 
 function love.update(dt)
     -- TODO: Point to all legs
-
-    target:setTarget(spider.centrePos, spider.legs[target.currentLeg])
     checkQuit()
+    for i=#anims, 1, -1 do
+        anims[i].anim:update(dt)
+        if not anims[i].anim.playing then
+            table.remove(anims, i)
+        end
+    end
 end
